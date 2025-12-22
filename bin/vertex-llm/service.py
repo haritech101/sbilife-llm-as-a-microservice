7e9ca23ad5b05f042bf4fdd31abbc44e6650ea3d
Harikrishna Natrajan <hari@tech101.in>
2025-12-22 14:10:37 +0530

    Microservice supports material reading and chunking

--- bin/vertex-llm/service.py
+++ bin/vertex-llm/service.py
@@ -1,9 +1,11 @@
 from asyncio import run, sleep
 from os import getenv
+from re import M
 from typing import NoReturn
 
 from dotenv import load_dotenv
 from sbilifeco.cp.llm.http_server import LLMHttpServer
+from sbilifeco.cp.material_reader.http_server import MaterialReaderHttpServer
 from sbilifeco.gateways.vertex_gemini import VertexGemini
 
 from envvars import Defaults, EnvVars
@@ -15,21 +17,30 @@ class VertexLLMMicroservice:
         region = getenv(EnvVars.vertex_ai_region, Defaults.vertex_ai_region)
         project_id = getenv(EnvVars.vertex_ai_project_id, "")
         model = getenv(EnvVars.vertex_ai_model, Defaults.vertex_ai_model)
-        http_port = int(getenv(EnvVars.http_port, Defaults.http_port))
+        http_port_qa = int(getenv(EnvVars.http_port_qa, Defaults.http_port_qa))
+        http_port_material = int(
+            getenv(EnvVars.http_port_material, Defaults.http_port_material)
+        )
 
         # Vertex gateway
-        self.llm = (
+        self.vertex = (
             VertexGemini()
             .set_region(region)
             .set_project_id(project_id)
             .set_model(model)
         )
-        await self.llm.async_init()
+        await self.vertex.async_init()
 
         # HTTP server
-        self.http_server = LLMHttpServer()
-        self.http_server.set_llm(self.llm).set_http_port(http_port)
-        await self.http_server.listen()
+        self.http_server_qa = LLMHttpServer()
+        self.http_server_qa.set_llm(self.vertex).set_http_port(http_port_qa)
+        await self.http_server_qa.listen()
+
+        self.http_server_material = MaterialReaderHttpServer()
+        self.http_server_material.set_material_reader(self.vertex).set_http_port(
+            http_port_material
+        )
+        await self.http_server_material.listen()
 
     async def run_forever(self) -> NoReturn:
         await self.start()
