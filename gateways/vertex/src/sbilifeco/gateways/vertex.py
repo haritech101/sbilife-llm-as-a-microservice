893b5023a12b8eff47c409c300c4128b7035f2eb
Harikrishna Natrajan <hari@tech101.in>
2026-04-02 08:25:08 +0530

    Gateway implements new contract with randomness

--- gateways/vertex/src/sbilifeco/gateways/vertex.py
+++ gateways/vertex/src/sbilifeco/gateways/vertex.py
@@ -3,7 +3,7 @@ from io import BufferedIOBase, RawIOBase, TextIOBase
 from typing import AsyncGenerator, AsyncIterator
 from traceback import format_exc
 from anthropic.types import DocumentBlockParam
-from sbilifeco.boundaries.llm import ILLM
+from sbilifeco.boundaries.llm import ILLM, LLMRequest
 from sbilifeco.models.base import Response
 from anthropic.lib.vertex import AsyncAnthropicVertex
 from anthropic.lib.streaming import AsyncMessageStream
@@ -82,13 +82,13 @@ class VertexAI(ILLM, BaseMaterialReader):
                 await vertex_client.close()
 
     async def generate_streamed_reply(
-        self, request_id: str, context: str
+        self, request: LLMRequest
     ) -> Response[AsyncGenerator[str, None]]:
         vertex_client: AsyncAnthropicVertex | None = None
 
         try:
             print(
-                f"Initializing Vertex AI client with region: {self.region}, project_id: {self.project_id}, model: {self.model} for request_id: {request_id}",
+                f"Initializing Vertex AI client with region: {self.region}, project_id: {self.project_id}, model: {self.model} for request_id: {request.request_id}",
                 flush=True,
             )
             vertex_client = AsyncAnthropicVertex(
@@ -96,7 +96,7 @@ class VertexAI(ILLM, BaseMaterialReader):
             )
 
             print(
-                f"Sending prompt and obtaining streamed response from Vertex AI for request_id: {request_id}",
+                f"Sending prompt and obtaining streamed response from Vertex AI for request_id: {request.request_id}",
                 flush=True,
             )
             reply = vertex_client.messages.stream(
@@ -104,16 +104,16 @@ class VertexAI(ILLM, BaseMaterialReader):
                 messages=[
                     {
                         "role": "user",
-                        "content": context,
+                        "content": request.context,
                     }
                 ],
                 model=self.model,
-                temperature=0,
+                temperature=request.randomness,
             )
 
             stream = await reply.__aenter__()
             print(
-                f"Stream obtained, processing streamed response for request_id: {request_id}",
+                f"Stream obtained, processing streamed response for request_id: {request.request_id}",
                 flush=True,
             )
 
@@ -141,13 +141,13 @@ class VertexAI(ILLM, BaseMaterialReader):
                         flush=True,
                     )
                     await vertex_client.close()
-                    del self.streams[request_id]
+                    del self.streams[request.request_id]
 
-            self.streams[request_id] = stream
-            return Response.ok(process_stream(request_id))
+            self.streams[request.request_id] = stream
+            return Response.ok(process_stream(request.request_id))
         except Exception as e:
             print(
-                f"Vertex Gateway: Error generating streamed reply with Vertex AI for request_id: {request_id}: {e}",
+                f"Vertex Gateway: Error generating streamed reply with Vertex AI for request_id: {request.request_id}: {e}",
                 flush=True,
             )
             print(format_exc(), flush=True)
