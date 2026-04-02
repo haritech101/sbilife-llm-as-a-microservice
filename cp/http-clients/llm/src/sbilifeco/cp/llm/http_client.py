8ddeecda188bc6d8fe0e8890866ffaddd379709a
Harikrishna Natrajan <hari@tech101.in>
2026-04-02 08:26:21 +0530

    HTTP controllers and presenters switch to new contract with randomness as input

--- cp/http-clients/llm/src/sbilifeco/cp/llm/http_client.py
+++ cp/http-clients/llm/src/sbilifeco/cp/llm/http_client.py
@@ -4,7 +4,7 @@ from traceback import format_exc
 from asyncio import get_running_loop
 from functools import partial
 from sbilifeco.cp.common.http.client import HttpClient
-from sbilifeco.boundaries.llm import ILLM, ChatMessage
+from sbilifeco.boundaries.llm import ILLM, ChatMessage, LLMRequest
 from sbilifeco.models.base import Response
 from sbilifeco.cp.llm.paths import Paths, LLMQuery
 from requests import Request, Session
@@ -24,13 +24,13 @@ class LLMHttpClient(HttpClient, ILLM):
             return Response.error(e)
 
     async def generate_streamed_reply(
-        self, request_id: str, context: str
+        self, request: LLMRequest
     ) -> Response[AsyncGenerator[str, None]]:
         try:
             req = Request(
                 method="POST",
-                url=f"{self.url_base}{Paths.STREAMS.format(request_id=request_id)}",
-                json=LLMQuery(context=context).model_dump(),
+                url=f"{self.url_base}{Paths.STREAMS}",
+                json=request.model_dump(),
             )
             with Session() as session:
                 prepped = session.prepare_request(req)
